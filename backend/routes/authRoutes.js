65901f96ef35c33e6695ef627766348c8e870a84
mehedi1505 <92613900+mehedi1505@users.noreply.github.com>
2023-11-22 01:22:53 +0600

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ router.get('/admin-logout', authControllers.admin_logout)
 // router.get('/get-user', authMiddleware, authControllers.get_user)
 // router.post('/sub-admin-register', authControllers.sub_admin_register)
 // router.post('/sub-admin-login', authControllers.sub_admin_login)
-router.get('/get-user-info',authMiddleware, authControllers.get_user)
+router.get('/get-user-info', authControllers.get_user)
 // router.post('/profile-image-upload', authMiddleware, authControllers.profile_image_upload)
 // router.post('/profile-info-add', authMiddleware, authControllers.profile_info_add)
 

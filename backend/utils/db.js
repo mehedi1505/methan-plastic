cb3813d8d7d30fbe12b3d75ce2806dc4da324857
mehedi1505 <92613900+mehedi1505@users.noreply.github.com>
2023-11-21 22:59:27 +0600

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
     module.exports.dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, { useNewURLParser: true,
-                                                    useUnifiedTopology: true })
+        await mongoose.connect(process.env.DB_URL, { useNewURLParser: true })
         .then(() => {console.log("Connected to Database");})
         .catch((err) => { console.log("Not Connected to Database ERROR! ", err);});
         // console.log("database connected....")

cc1c93c50ca18326646dfb12ee37a4f065338b76
mehedi1505 <92613900+mehedi1505@users.noreply.github.com>
2023-11-22 12:04:21 +0600

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 module.exports.authMiddleware = async(req, res, next)=>{
         const { accessToken } = req.cookies
         if(!accessToken){
-                return res.status(409).json({error:'Please login first'})
+                  next();
         }else{
             try {
                 const decodedToken = await jwt.verify(accessToken,process.env.SECRET);

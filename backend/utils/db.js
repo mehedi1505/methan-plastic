59e2a331d96524e54e19e8c9a85bf3e082c5be0d
mehedi1505 <92613900+mehedi1505@users.noreply.github.com>
2023-12-05 14:26:01 +0600

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
     module.exports.dbConnect = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL, { useNewURLParser: true })
-        .then(() => {console.log("Connected to Database");})
-        .catch((err) => { console.log("Not Connected to Database ERROR! ", err);});
-        // console.log("database connected....")
+     try {
+        if(process.env.MODE === 'pro'){
+            await mongoose.connect(process.env.DB_PRO_URL, {useNewURLParser: true})
+            .then(() => {console.log("Connected to Production Database");})
+            .catch((err) => { console.log("Not connected to database ERROR! ", err);});
+        }else{
+            await mongoose.connect(process.env.DB_LOCAL_URL, {useNewURLParser: true})
+            .then(() => {console.log("Connected to local Database");})
+            .catch((err) => { console.log("Not connected to database ERROR! ", err);});
+        }
     } catch (error) {
         console.log(error.message)
     }
